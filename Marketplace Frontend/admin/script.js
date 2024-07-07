523e43e3ed9f1f4567494091e67a352331dea172
ananyakundu23 <166933344+ananyakundu23@users.noreply.github.com>
2024-07-07 23:16:01 +0530

    Add files via upload

---
+++ Marketplace Frontend/admin/script.js
@@ -0,0 +1,106 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
+
+
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+})
+
+
+
+
+
+
+
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+	if(window.innerWidth < 576) {
+		e.preventDefault();
+		searchForm.classList.toggle('show');
+		if(searchForm.classList.contains('show')) {
+			searchButtonIcon.classList.replace('bx-search', 'bx-x');
+		} else {
+			searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		}
+	}
+})
+
+
+
+
+
+if(window.innerWidth < 768) {
+	sidebar.classList.add('hide');
+} else if(window.innerWidth > 576) {
+	searchButtonIcon.classList.replace('bx-x', 'bx-search');
+	searchForm.classList.remove('show');
+}
+
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-x', 'bx-search');
+		searchForm.classList.remove('show');
+	}
+})
+
+
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+})
+
+
+// CONTENT CHANGE 
+
+function showContent(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => section.classList.add('hidden')); // Rename the loop variable to section
+
+    const activeSelection = document.getElementById(sectionId);
+    activeSelection.classList.remove('hidden');
+}
+
+function addProduct() {
+    const productList = document.getElementById('product-list');
+    const newRow = document.createElement('tr');
+
+    newRow.innerHTML = `
+        <td>New Product</td>
+        <td>$0</td>
+        <td><button onclick="deleteProduct(this)">Delete</button></td>
+    `;
+
+    productList.appendChild(newRow);
+}
+
+function deleteProduct(button) {
+    const row = button.parentElement.parentElement;
+    row.remove();
+}
